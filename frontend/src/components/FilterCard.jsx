dd5f84b81835ed561f022caeaa0dbe8230b9f8d6
ShiniThinh <slimerimuru2005@gmail.com>
2025-05-10 10:51:30 +0700

    a

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,48 +1,43 @@
-import React from 'react'
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Label } from "./ui/label"
+import React from "react";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "./ui/label";
 const filterData = [
-    {
-        filterType: "Location",
-        array: ["Vietnam", "USA", "UK", "Canada"]
-    },
-    {
-        filterType: "Industry",
-        array: ["Frontend Developer", "Backend Developer", "Fullstack Developer"]
-    },
-    {
-        filterType: "Salary",
-        array: ["5-10tr VNĐ", "10-15tr VNĐ", "15-20tr VNĐ", "20-30tr VNĐ"]
-    }
-]
+  {
+    filterType: "Location",
+    array: ["Vietnam", "USA", "UK", "Canada"],
+  },
+  {
+    filterType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "Fullstack Developer"],
+  },
+  {
+    filterType: "Salary",
+    array: ["5-10tr VNĐ", "10-15tr VNĐ", "15-20tr VNĐ", "20-30tr VNĐ"],
+  },
+];
 
 const FilterCard = () => {
-    return (
-        <div className='w-full bg-white p-3 rounded-md'>
-            <h1 className='font-bold text-lg'>Filter</h1>
-            <hr className='mt-3' />
-            <RadioGroup >
-                {
-                    filterData.map((data, index) => (
-                        <div>
-                            <h1 className='font-bold text-lg'>{data.filterType}</h1>
-                            {
-                                data.array.map((item, index) => {
-                                    return (
-                                        <div className='flex items-center space-x-2 my-2' key={index}>
-                                            <RadioGroupItem value={item} />
-                                            <Label>{item}</Label>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    ))
-                }
-            </RadioGroup>
+  return (
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter</h1>
+      <hr className="mt-3" />
+      <RadioGroup>
+        {filterData.map((data, index) => (
+          <div>
+            <h1 className="font-bold text-lg">{data.filterType}</h1>
+            {data.array.map((item, index) => {
+              return (
+                <div className="flex items-center space-x-2 my-2" key={index}>
+                  <RadioGroupItem value={item} />
+                  <Label>{item}</Label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default FilterCard
+export default FilterCard;
